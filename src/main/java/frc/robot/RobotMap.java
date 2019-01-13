4678a1a81f866fcaa7619aae0edec0a2ba9c05a1
Kamil-HomePC <nfkamil@hotmail.com>
2019-01-13 16:35:00 -0500

    idk what I did just fixing it

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -20,8 +20,23 @@ public class RobotMap {
   }
   public static enum Controller {
     DRIVER_CONTROLLER(0),
+<<<<<<< HEAD
     OPERATOR_CONTROLLER(1);
 
+=======
+    OPERATOR_CONTROLLER(1),
+    BUTTON_A(1),
+    BUTTON_B(2),
+    BUTTON_X(3),
+    BUTTON_Y(4),
+    BUTTON_LB(5),
+    BUTTON_RB(6),
+    BUTTON_BACK(7),
+    BUTTON_START(8),
+    STICK_LEFT(9),
+    STICK_RIGHT(10);
+    
+>>>>>>> parent of 7bc598d... Revert "K0.1.2.1 Minor Fixes"
     private int value;
 
     private Controller(int value) {
