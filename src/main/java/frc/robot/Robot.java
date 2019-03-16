3b9b344a6f7047a69c988192720e6eb40227d875
Kamil <nfkamil@hotmail.com>
2019-03-15 23:19:41 -0500

    Revert "Robot.java"
    This reverts commit cfdfe8f793989fdafc00ef78a92951c5a990b599.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
@@ -31,7 +30,6 @@ import com.kauailabs.navx.frc.AHRS;
  */
 public class Robot extends TimedRobot {
   public static DriveSubsystem DRIVE_SUBSYSTEM = new DriveSubsystem();
-  public static ElevatorSubsystem ELEVATOR_SUBSYSTEM = new ElevatorSubsystem();
   public static ClimbSubsystem climb = new ClimbSubsystem();
   public static OI m_oi;
 
